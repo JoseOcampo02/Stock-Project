c1a2f5d7596285e859954c73bb3200d72fe131e4
PERMAPOG <85780059+PERMAPOG@users.noreply.github.com>
2023-04-12 20:13:36 -0700

    GUI + LINECHARTS! 8:13pm 4/12/23

--- src/main/java/SampleController.java
+++ src/main/java/SampleController.java
@@ -1,4 +1,3 @@
-
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Label;
@@ -27,7 +26,7 @@ import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
 import yahoofinance.histquotes.Interval;
 import javafx.scene.control.Tooltip;
-
+import javafx.geometry.Point2D;
 
 
 //import com.example.main.Main;
@@ -40,10 +39,8 @@ public class SampleController implements Initializable {
     @FXML
     private LineChart<Number, Number> priceChart;
     
-    @FXML
-    private Label hoverLabel;
-    @FXML
-    private Label hoverLabel2;
+
+    
     
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -72,12 +69,6 @@ public class SampleController implements Initializable {
 	    
 	    
 	    
-	    Series<Number, Number> priceLine = new XYChart.Series<>();
-	    priceLine.setName("Price");
-	    for (int i = 0; i < realData.size(); i++) {
-	    	priceLine.getData().add(new Data<Number, Number>(i + 1, realData.get(i)));
-	    }
-	    
 	    
 	    
 	    //offsets for lines
@@ -87,6 +78,14 @@ public class SampleController implements Initializable {
 	    
 	    
 	    // structure for line chart (individual line)  
+	    
+	    
+	    Series<Number, Number> priceLine = new XYChart.Series<>();
+	    priceLine.setName("Price");
+	    for (int i = 0; i < realData.size(); i++) {
+	    	priceLine.getData().add(new Data<Number, Number>(i + 1, realData.get(i)));
+	    }
+	    
 	    Series<Number, Number> histogramLine = new XYChart.Series<>();
 	    histogramLine.setName("Histogram");
 	    
@@ -123,47 +122,18 @@ public class SampleController implements Initializable {
 
 	   
 
+
+	    
 	}
 	
 	
 	
+
+
+
 	
-	
-	 @FXML
-	 public void handleMouseMoved(MouseEvent event) {
-		    double x = event.getX();
-		    double y = event.getY();
-
-		    // Retrieve the data from the chart
-		    NumberAxis xAxis = (NumberAxis) priceChart.getXAxis();
-		    NumberAxis yAxis = (NumberAxis) priceChart.getYAxis();
-
-		    double xValue = xAxis.getValueForDisplay(x).doubleValue();
-		    double yValue = yAxis.getValueForDisplay(y).doubleValue();
-
-		    // Find the closest data point to the mouse pointer
-		    double minDistance = Double.MAX_VALUE;
-		    Data<Number, Number> closestDataPoint = null;
-		    for (XYChart.Series<Number, Number> series : priceChart.getData()) {
-		        for (XYChart.Data<Number, Number> data : series.getData()) {
-		            double distance = Math.abs(data.getXValue().doubleValue() - xValue);
-		            if (distance < minDistance) {
-		                minDistance = distance;
-		                closestDataPoint = data;
-		            }
-		        }
-		    }
-
-		    // Update the hoverLabel with the hovered values
-		    String labelText = String.format("X: %.2f, Y: %.2f", xValue, yValue);
-		    if (closestDataPoint != null) {
-		        labelText += String.format(", Closest Data Point: %.2f", closestDataPoint.getYValue().doubleValue());
-		    }
-		    hoverLabel.setText(labelText);
-		}
-
-
 	
 	
 	
 }
+	
