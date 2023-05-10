107aaeca65c8723966681ab1c82da79c978e37d6
Alonzo G. <124635098+0xalonzo@users.noreply.github.com>
2023-05-09 21:05:26 -0700

    Added User name input, user number of TSLA shares, and a text box that displays the information and the total worth of the users' shares. Updated profileview.fxml and profileViewController.java

--- src/main/java/profileViewController.java
+++ src/main/java/profileViewController.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.TextArea;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +12,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 public class profileViewController implements Initializable {
 
@@ -20,15 +24,38 @@ public class profileViewController implements Initializable {
 
     @FXML
     private TextField sharesTextField;
-    
+
     @FXML
     private Text numberText;
 
     private String[] userData;
+    
+    @FXML
+    private TextArea consoleOutputTextArea;
+    
+    @FXML
+    private Button saveButton;
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         userData = new String[2];
+        
+        redirectConsoleOutputToTextArea();
+        
+    }
+    
+    private void redirectConsoleOutputToTextArea() {
+        OutputStream outputStream = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                consoleOutputTextArea.appendText(String.valueOf((char) b));
+            }
+        };
+
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        System.setErr(printStream);
     }
 
     public void returnToHomeView(ActionEvent event) throws IOException {
@@ -38,20 +65,32 @@ public class profileViewController implements Initializable {
         window.setScene(profileViewScene);
         window.show();
     }
+    
+    double product;
+    
+    // temporary price
+    double closingPrice = 169.15;
+    
+    private double calculateProduct(int shares, double closingPrice) {
+        product = shares * closingPrice;
+    	return product;
+    }
 
     public void saveUserInfo(ActionEvent event) {
-    	int totalInv;
-        // Another way to store data? database
         userData[0] = nameTextField.getText();
         userData[1] = sharesTextField.getText();
-        System.out.println("User: " + userData[0] + "\nShares: " + userData[1]);
         
-        //totalInv = userData[1] * 
+        int shares = Integer.parseInt(userData[1]);
+        double product = calculateProduct(shares, closingPrice);
         
-        
-        
+        System.out.println("Hello,  " + userData[0]);
+        System.out.println("You currently have " + userData[1] + " shares of");
+        System.out.println("TSLA worth $" + product);
+
         // Set the number text to the value entered in the sharesTextField
         numberText.setText(sharesTextField.getText());
     }
 
+
+    
 }
